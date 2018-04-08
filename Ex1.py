ad865b1d771e798bb675847520f65fc32dd3306b
Shaked Hazon <shkdhazon@gmail.com>
2018-04-08 18:33:41 +0300

    Final?

--- Ex1.py
+++ Ex1.py
@@ -1,28 +1,35 @@
 import os
 import sys
 import re
-import ast
 
 delimiter = r"::"
 
 
+def string_to_var(value):
+    assert value, "Problem parsing value"
+    try:
+        return eval(value)
+    except Exception:
+        return value
+
+
 def union(*args):
     args = args[0]  # Extract arguments list from *args tuple
-    input_validation(args, is_union=True)
+    union_separate_input_validation(args, is_union=True)
     tables_structure_validation(args[0], args[1])
+    unique_val = 0
     with open(args[2], "w") as output_file:
+        unique_val += 1
         for input_file_path in [args[0], args[1]]:
-            file_name = os.path.basename(input_file_path)
             with open(input_file_path, "r") as input_file:
                 for line in input_file:
                     line = line.strip()
-                    # ToDo (overkill): if the files have the same base file name, then it won't be a unique value
-                    output_file.write("{0}{1}{2}\n".format(line, delimiter, file_name))
+                    output_file.write("{0}{1}{2}\n".format(line, delimiter, unique_val))
 
 
 def separate(*args):
     args = args[0]  # Extract arguments list from *args tuple
-    input_validation(args, is_union=False)
+    union_separate_input_validation(args, is_union=False)
     identifier_to_file = {}
     current_file = 0
     with open(args[1], "w") as output_file_1:
@@ -32,7 +39,7 @@ def separate(*args):
                 for line in input_file:
                     split_line = re.split(delimiter, line)
                     identifier = split_line[-1].rstrip()  # remove /n
-                    if identifier not in identifier_to_file.keys():
+                    if identifier not in identifier_to_file:
                         assert current_file <= 1, 'Found more than two distinct file identifiers: {0},{1}'.format(
                             ",".join(identifier_to_file.keys()), identifier)
                         identifier_to_file[identifier] = files[current_file]
@@ -41,7 +48,7 @@ def separate(*args):
                     identifier_to_file[identifier].write(new_line)
 
 
-def input_validation(args, is_union):
+def union_separate_input_validation(args, is_union):
     assert len(args) == 3, "Wrong number of arguments. This operation requires 3 arguments"
 
     assert os.path.isfile(args[0]) and (not is_union or os.path.isfile(args[1])), "Input arguments don't exist"
@@ -58,7 +65,7 @@ def input_validation(args, is_union):
 
 def distinct(*args):
     args = args[0]
-    validate_input_distinct(args)
+    distinct_input_validation(args)
     input_file_path = args[0]
     column_index = int(args[1])
     output_file_path = args[2]
@@ -75,30 +82,22 @@ def distinct(*args):
             output_file.write(str(val) + '\n')
 
 
-def string_to_var(value):
-    assert value is not None, "Problem parsing value"
-    try:
-        return int(value)
-    except ValueError:
-        return value
-
-
 def sort_with_type(result_set):
     first_val = next(iter(result_set))
     # For numbers, sort normally:
-    if type(first_val) is int:
+    if isinstance(type(first_val), int):
         return sorted(result_set)
-    # For lists, don't sort:
-    if isinstance(first_val, basestring) and '{' in first_val:
-        return result_set
     # For strings, sort with no case:
-    return sorted(result_set, key=lambda v: (v.upper(), v[0].islower()))
+    if isinstance(first_val, basestring):
+        return sorted(result_set, key=lambda v: (v.upper(), v[0].islower()))
+    # For lists, don't sort:
+    return result_set
 
 
-def validate_input_distinct(args):
+def distinct_input_validation(args):
     assert len(args) == 3, "Wrong number of arguments. This operation requires 3 arguments"
     assert os.path.isfile(args[0]), "Input file does not exist"
-    assert type(ast.literal_eval(args[1])) is int and int(args[1]) >= 0, "Index must be non-negative integer"
+    assert isinstance(type(string_to_var(args[1])), int) and int(args[1]) >= 0, "Index must be non-negative integer"
     assert os.path.splitext(args[0])[1] == os.path.splitext(args[2])[1], "Files must be the same type"
     with open(args[0], "r") as input_file:
         assert len(re.split(delimiter, input_file.next())) > int(args[1]), "Column does not exist in table"
@@ -109,7 +108,7 @@ def validate_input_distinct(args):
 
 def like(*args):
     args = args[0]
-    validate_input_like(args)
+    like_input_validation(args)
     input_file_path = args[0]
     column_index = int(args[1])
     regex = args[2]
@@ -127,10 +126,10 @@ def like(*args):
             output_file.write(str(line))
 
 
-def validate_input_like(args):
+def like_input_validation(args):
     assert len(args) == 4, "Wrong number of arguments. This operation requires 4 arguments"
     assert os.path.isfile(args[0]), "Input file does not exist"
-    assert type(ast.literal_eval(args[1])) is int and int(args[1]) >= 0, "Index must be non-negative integer"
+    assert isinstance(type(string_to_var(args[1])), int) and int(args[1]) >= 0, "Index must be non-negative integer"
     assert os.path.splitext(args[0])[1] == os.path.splitext(args[3])[1], "Files must be the same type"
     try:
         re.compile(args[2])
@@ -141,22 +140,15 @@ def validate_input_like(args):
 
 
 def tables_structure_validation(file_1_path, file_2_path):
-    # ToDo: can we use numpy? what is eval?
-    import numpy as np
-    from string import digits
-    # https://penandpants.com/2012/03/09/reading-text-tables-with-python/
-
-    file_1_structure = np.genfromtxt(file_1_path, delimiter=delimiter, dtype=None).dtype
-    file_2_structure = np.genfromtxt(file_2_path, delimiter=delimiter, dtype=None).dtype
-    error_message = "The tables' format does not match"
-
-    assert len(file_1_structure) == len(file_2_structure), error_message
-
-    for i in range(len(file_1_structure.fields)):
-        # Extract dtype name of column i and remove digits from it (so that 'int64' == 'int32')
-        file_1_col_i_type = file_1_structure.fields.values()[i][0].name.translate(None, digits)
-        file_2_col_i_type = file_2_structure.fields.values()[i][0].name.translate(None, digits)
-        assert file_1_col_i_type == file_2_col_i_type, error_message
+    with open(file_1_path, "r") as file_1:
+        line_1 = file_1.next()
+    with open(file_2_path, "r") as file_2:
+        line_2 = file_2.next()
+    split_1 = re.split(delimiter, line_1)
+    split_2 = re.split(delimiter, line_2)
+    assert len(split_1) == len(split_2), "Files don't have same number of columns"
+    for i in range(len(split_1)):
+        assert type(string_to_var(split_1[i])) == type(string_to_var(split_2[i])), "The tables' format does not match"
 
 
 if __name__ == "__main__":
@@ -165,10 +157,10 @@ if __name__ == "__main__":
             {
                 "UNION": union,
                 "SEPARATE": separate,
+                "SEPERATE": separate,
                 "DISTINCT": distinct,
                 "LIKE": like
             }
-        # ToDo: in his examples it says seperate
 
         assert len(sys.argv) >= 2, "Missing operation. Available operations: {0}".format(
             ", ".join(functions_dict.keys()))
