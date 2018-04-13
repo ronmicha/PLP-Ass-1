2a3f14b44c9ccb92e62877b2ee3ab7884b1b2055
Ron Michaeli <ronmicha@post.bgu.ac.il>
2018-04-13 14:22:45 +0300

    In progress...

--- Lab1/backend.py
+++ Lab1/backend.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import sqlite3
 from flask import Flask
 from flask import jsonify
@@ -11,7 +12,7 @@ ratings_path = "ratings.csv"
 app = Flask(__name__)
 
 
-def create_table():
+def create_movies_table():
     if not os.path.isfile(db_path):
         with sqlite3.connect(db_path) as connection:
             connection.text_factory = str
@@ -20,17 +21,16 @@ def create_table():
             cursor.execute(sql_command)
             connection.commit()
             with open(movies_path, "r") as movies_file:
-                next(movies_file)  # skip header row
+                reader = csv.reader(movies_file)
+                next(reader)  # skip header row
                 movies_list = []
-                for row in movies_file:
-                    try:  # TODO: Some rows have comma in "movieId" column. Ask Nahmias
-                        movie_id, title, genre = row.strip().split(",")
-                        title = title.split("(")[0].strip()
-                        genre = genre.split("|")[0].strip()
-                        movies_list.append((movie_id, title, genre))
-                    except:
-                        pass
-            sql_command = "INSERT INTO movies (ID, Title, Genre) VALUES (?, ?, ?)"
+                for row in reader:
+                    movie_id = row[0]
+                    title = row[1]
+                    genre = row[2].split("|")[0].strip()
+                    movies_list.append((movie_id, title, genre))
+            sql_command = "INSERT INTO movies (ID, Title, Genre) " \
+                          "VALUES (?, ?, ?)"
             cursor.executemany(sql_command, movies_list)
             connection.commit()
 
@@ -38,18 +38,39 @@ def create_table():
 def get_all_movies():
     with sqlite3.connect(db_path) as connection:
         cursor = connection.cursor()
-        sql_command = "SELECT Title FROM movies ORDER BY Title"
+        sql_command = "SELECT * " \
+                      "FROM movies " \
+                      "ORDER BY Title"
         cursor.execute(sql_command)
-        all_movies = [title_tuple[0] for title_tuple in cursor.fetchall()]
+        all_movies = []
+        for row in cursor.fetchall():
+            row_list = list(row)
+            row_list[0] = str(row_list[0])
+            all_movies.append(" | ".join(row_list))
     return all_movies
 
 
 def search_movies(movie_id, title, genre):
-    pass
+    with sqlite3.connect(db_path) as connection:
+        cursor = connection.cursor()
+        sql_command = "SELECT * " \
+                      "FROM movies " \
+                      "WHERE ID = {0} AND Title = {1} AND Genre = {2} " \
+                      "ORDER BY Title".format(movie_id, title, genre)
+        cursor.execute(sql_command)
+        movies = []
+        for row in cursor.fetchall():
+            row_list = list(row)
+            row_list[0] = str(row_list[0])
+            movies.append(" | ".join(row_list))
+    return movies
 
 
 def add_movie(movie_id, title, genre):
-    pass
+    with sqlite3.connect(db_path) as connection:
+        cursor = connection.cursor()
+        sql_command = "INSERT INTO movies VALUES (?, ?, ?)"
+        cursor.execute(sql_command, (movie_id, title, genre))
 
 
 def update_movie(movie_id, title, genre):
--- Lab1/frontend.py
+++ Lab1/frontend.py
@@ -1,5 +1,6 @@
 import backend
 from Tkinter import *
+from tkMessageBox import *
 
 
 def get_all_movies():
@@ -9,12 +10,21 @@ def get_all_movies():
 
 
 def search_movies(movie_id, title, genre):
-    movies = backend.search_movies(movie_id.get(), title.get(), genre.get())
+    movie_id = "ID" if not movie_id.get() else movie_id.get()
+    title = "Title" if not title.get() else "'{}'".format(title.get())
+    genre = "Genre" if not genre.get() else "'{}'".format(genre.get())
+    movies = backend.search_movies(movie_id, title, genre)
     fill_listbox(movies)
 
 
 def add_movie(movie_id, title, genre):
-    backend.add_movie(movie_id.get(), title.get(), genre.get())
+    movie_id = movie_id.get()
+    title = title.get()
+    genre = genre.get()
+    if not movie_id or not title or not genre:
+        showerror("Error - Missing Values", "Please fill \"Title\", \"ID\", \"Genre\" fields and try again.")
+        return
+    backend.add_movie(movie_id, title, genre)
 
 
 def update_movie(new_movie_id, new_title, new_genre):
@@ -27,7 +37,7 @@ def delete_movie(movie_id):
 
 
 def close():
-    pass
+    window.destroy()
 
 
 def fill_listbox(values):
@@ -36,13 +46,14 @@ def fill_listbox(values):
         movies_listbox.insert(END, value)
 
 
-backend.create_table()
+backend.create_movies_table()
 
 # region Tkinter GUI
 window = Tk()
+window.geometry("500x300")
 
 title_var = StringVar()
-id_var = IntVar()
+id_var = StringVar()
 genre_var = StringVar()
 
 title_label = Label(window, text="Title")
@@ -55,6 +66,7 @@ genre_entry = Entry(window, textvariable=genre_var)
 
 scrollbar = Scrollbar(window, orient=VERTICAL)
 movies_listbox = Listbox(window, selectmode=SINGLE, yscrollcommand=scrollbar.set)
+# movies_listbox.bind("<<ListboxSelect>>", movies_listbox_on_select)
 scrollbar.config(command=movies_listbox.yview)
 
 view_all_button = Button(window, text="View all", command=lambda: get_all_movies())
@@ -64,20 +76,30 @@ update_button = Button(window, text="Update selected", command=lambda: update_mo
 delete_button = Button(window, text="Delete selected", command=lambda: delete_movie(id_var))
 close_button = Button(window, text="Close", command=close)
 
-title_label.grid(row=0, column=1)
-title_entry.grid(row=0, column=2)
-id_label.grid(row=1, column=1)
-id_entry.grid(row=1, column=2)
-genre_label.grid(row=0, column=3)
-genre_entry.grid(row=0, column=4)
-movies_listbox.grid(row=3, column=0, columnspan=3)
-scrollbar.grid(row=4, column=3)
-view_all_button.grid(row=2, column=4)
-search_button.grid(row=3, column=4)
-add_button.grid(row=4, column=4)
-update_button.grid(row=5, column=4)
-delete_button.grid(row=6, column=4)
-close_button.grid(row=7, column=4)
+num_of_rows = 10
+num_of_columns = 15
+
+for i in range(num_of_rows):
+    window.rowconfigure(i, weight=1)
+for i in range(num_of_columns):
+    window.columnconfigure(i, weight=1)
+
+button_column = 12
+
+title_label.grid(row=0, column=2)
+title_entry.grid(row=0, column=3, sticky="EW")
+id_label.grid(row=1, column=2)
+id_entry.grid(row=1, column=3, sticky="EW")
+genre_label.grid(row=0, column=8)
+genre_entry.grid(row=0, column=9, sticky="EW")
+movies_listbox.grid(row=3, column=0, columnspan=10, rowspan=4, sticky="NESW")
+scrollbar.grid(row=3, column=10, rowspan=4, sticky="NS")
+view_all_button.grid(row=2, column=button_column, sticky="EW", padx=(10, 10))
+search_button.grid(row=3, column=button_column, sticky="EW", padx=(10, 10))
+add_button.grid(row=4, column=button_column, sticky="EW", padx=(10, 10))
+update_button.grid(row=5, column=button_column, sticky="EW", padx=(10, 10))
+delete_button.grid(row=6, column=button_column, sticky="EW", padx=(10, 10))
+close_button.grid(row=7, column=button_column, sticky="EW", padx=(10, 10))
 
 window.mainloop()
 # endregion
