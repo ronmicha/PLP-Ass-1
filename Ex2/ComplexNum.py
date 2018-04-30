35049bc7a54e38cd602801b16306006545cefaa0
Ron Michaeli <ronmicha@post.bgu.ac.il>
2018-04-30 10:33:27 +0300

    Ex2 here we go...

--- Ex2/ComplexNum.py
+++ Ex2/ComplexNum.py
@@ -1,3 +1,37 @@
 class ComplexNum:
-    def __init__(self):
+    def __init__(self, re, im):
+        self._re = re
+        self._im = im
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __neg__(self):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def __abs__(self):
+        pass
+
+    def re(self):
+        pass
+
+    def im(self):
+        pass
+
+    def to_tuple(self):
+        pass
+
+    def conjugate(self):
         pass
