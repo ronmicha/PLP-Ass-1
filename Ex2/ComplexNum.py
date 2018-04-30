dee641d14e8eeacb60601100c328c4a616d34581
Ron Michaeli <ronmicha@post.bgu.ac.il>
2018-04-30 11:17:09 +0300

    __init__, __repr__, re, im, to_tuple - done!

--- Ex2/ComplexNum.py
+++ Ex2/ComplexNum.py
@@ -1,10 +1,24 @@
 class ComplexNum:
     def __init__(self, re, im):
+        assert isinstance(re, (float, int)), "Real number is not a number"
+        assert isinstance(im, (float, int)), "Imaginary number is not a number"
         self._re = re
         self._im = im
 
     def __repr__(self):
-        pass
+        if self._re == 0 and self._im == 0:
+            return "0"
+        re_str = "" if self._re == 0 else str(self._re)
+        im_str = "" if self._im == 0 else str(abs(self._im)) + "i"
+        if self._re != 0 and self._im > 0:
+            sign = " + "
+        elif self._re != 0 and self._im < 0:
+            sign = " - "
+        elif self._re == 0 and self._im < 0:
+            sign = "-"
+        else:
+            sign = ""
+        return "{0}{1}{2}".format(re_str, sign, im_str)
 
     def __eq__(self, other):
         pass
@@ -25,13 +39,13 @@ class ComplexNum:
         pass
 
     def re(self):
-        pass
+        return self._re
 
     def im(self):
-        pass
+        return self._im
 
     def to_tuple(self):
-        pass
+        return self._re, self._im
 
     def conjugate(self):
         pass
