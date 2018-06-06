e93c380c1dfcf6cd145cb1280dd3120138468277
Ron Michaeli <ronmicha@post.bgu.ac.il>
2018-06-06 21:48:31 +0300

    Part1

--- Ex3/Part1.py
+++ Ex3/Part1.py
@@ -5,27 +5,37 @@ import pandas as pd
 
 
 # https://stackoverflow.com/questions/45835993/groupby-and-reduce-pandas-dataframes-with-numpy-arrays-as-entries
+# problems with this solution:
+# 1. Output values are not delimited with comma
+# 2. Long output values are cut off or '...'
+# 3. Column order is arbitrary (easy fix)
+
 def create_user_profiles(ratings_df, output_directory):
-    # problems with this solution:
-    # 1. Output values are not delimited with comma
-    # 2. Long output values are cut off or '...'
-    user_profiles_df = pd.DataFrame(columns=["userId", "movieId", "rating"])
-    user_profiles_df["userId"] = ratings_df["userId"].unique()
-    user_profiles_df["movieId"] = ratings_df.groupby("userId")["movieId"].apply(np.hstack).values
-    user_profiles_df["rating"] = ratings_df.groupby("userId")["rating"].apply(np.hstack).values
-    user_profiles_df.to_csv(output_directory + r"user profiles.csv", index=False, mode='a')
+    ratings_df = ratings_df.sort_values("userId")
+    user_profiles_df = pd.DataFrame({
+        "userId": ratings_df["userId"].unique(),
+        "movieIds": ratings_df.groupby("userId")["movieId"].apply(np.hstack).values,
+        "ratings": ratings_df.groupby("userId")["rating"].apply(np.hstack).values
+    })
+    user_profiles_df.to_csv(output_directory + r"user profiles.csv", index=False)
 
 
 def create_item_profile(ratings_df, output_directory):
-    pass
+    ratings_df = ratings_df.sort_values("movieId")
+    item_profiles_df = pd.DataFrame({
+        "movieId": ratings_df["movieId"].unique(),
+        "userIds": ratings_df.groupby("movieId")["userId"].apply(np.hstack).values,
+        "ratings": ratings_df.groupby("movieId")["rating"].apply(np.hstack).values
+    })
+    item_profiles_df.to_csv(output_directory + r"item profiles.csv", index=False)
 
 
 if __name__ == '__main__':
     try:
-        # assert len(sys.argv) == 5, "Wrong number of arguments provided. Please pass 4 arguments."
+        # assert len(sys.argv) == 5, "Wrong number of arguments provided. Please provide 4 arguments"
         # assert sys.argv[1] == "ExtractProfiles", "Wrong method provided. Method must be 'ExtractProfiles'"
-        # assert os.path.basename(sys.argv[2]) == "ratings.csv", "Wrong input file provided. Input file must be 'ratings.csv'"
-        # assert os.path.isfile(sys.argv[2]), "Input file path is invalid or doesn't exist"
+        # assert os.path.basename(sys.argv[2]) == "ratings.csv", "Wrong input file provided. Input file must be named 'ratings.csv'"
+        # assert os.path.isfile(sys.argv[2]), "Input file path is invalid or file doesn't exist"
         # assert os.path.isdir(sys.argv[3]), "User Profiles directory is invalid or doesn't exist"
         # assert os.path.isdir(sys.argv[4]), "Item Profiles directory is invalid or doesn't exist"
         #
@@ -37,8 +47,8 @@ if __name__ == '__main__':
         user_profiles_directory = ""
         items_profiles_directory = ""
 
-        ratings_df = pd.read_csv(ratings_path)
-        create_user_profiles(ratings_df, user_profiles_directory)
-        create_item_profile(ratings_df, items_profiles_directory)
+        rating_df = pd.read_csv(ratings_path)
+        create_user_profiles(rating_df, user_profiles_directory)
+        create_item_profile(rating_df, items_profiles_directory)
     except Exception as ex:
         print "ERROR!", ex.message
