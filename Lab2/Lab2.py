13c82565634048b1864eee0c0e235ca498308ede
Shaked Hazon <shkdhazon@gmail.com>
2018-07-13 16:27:44 +0300

    End of part A?

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,15 +1,20 @@
+import math
+
 import pandas as pd
 import datetime as dt
 from difflib import SequenceMatcher
 from pandas.tseries.offsets import MonthEnd
 
 # region Column Names Enum
+CATEGORY = "category"
 USERID = "userId"
 WEEKDAY = "weekday"
 DATE = "date"
 INCOME = "income"
 AMOUNT = "amount"
 NAME = "name"
+CATEGORYID = "categoryId"
+SUBSCRIPTION = "subscription"
 TOTAL_INCOMES_LAST_WEEK = "total_incomes_last_week"
 NUM_OF_INCOMES_LAST_WEEK = "num_of_incomes_last_week"
 TOTAL_INCOMES_LAST_MONTH = "total_incomes_last_month"
@@ -32,15 +37,20 @@ def read_users_and_transactions_files(users_file_path, transactions_file_path):
     users_df = pd.read_json(users_file_path)
     global transactions_df
     transactions_df = pd.read_json(transactions_file_path)
-    transactions_df = transactions_df.sort_values("date")
+    transactions_df = transactions_df.sort_values(DATE)
 
 
 def part_a():
     transactions_df[WEEKDAY] = pd.to_datetime(transactions_df[DATE]).apply(lambda x: x.weekday())
+    categories_df = transactions_df[CATEGORY].apply(lambda x: pd.Series({k: 1 for v, k in enumerate(x)})).fillna(0)
+    global all_categories
+    all_categories = list(categories_df.columns)
+    global transactions_df
+    transactions_df = pd.concat([transactions_df, categories_df], axis=1)
 
     # income prediction features
     transactions_df[INCOME] = transactions_df[AMOUNT] < 0
-    incomes_data = []
+    additional_features_data = []
     for index, row in transactions_df.iterrows():
         last_week_incomes = get_last_n_days_incomes(row, 7)
         total_incomes_last_week = round(-last_week_incomes[AMOUNT].sum(), 2) if not last_week_incomes.empty else None
@@ -49,25 +59,42 @@ def part_a():
         total_incomes_last_month = round(-last_month_incomes[AMOUNT].sum(),
                                          2) if not last_month_incomes.empty else None
         num_of_incomes_last_month = len(last_month_incomes.index)
-        same_name_last_week = same_attr_past_n_days(row, NAME, 7, 0.7)
-        incomes_data.append(
-            [total_incomes_last_week, num_of_incomes_last_week, total_incomes_last_month, num_of_incomes_last_month])
-    incomes_df = pd.DataFrame(incomes_data, columns=[TOTAL_INCOMES_LAST_WEEK,
-                                                     NUM_OF_INCOMES_LAST_WEEK,
-                                                     TOTAL_INCOMES_LAST_MONTH,
-                                                     NUM_OF_INCOMES_LAST_MONTH])
+        same_name_last_week = same_attr_past_n_days(row, NAME, 7,
+                                                    lambda a, b: SequenceMatcher(None, a, b).ratio() >= 0.7)
+        same_name_last_month = same_attr_past_n_days(row, NAME, 30,
+                                                     lambda a, b: SequenceMatcher(None, a, b).ratio() >= 0.7)
+        same_amount_last_week = same_attr_past_n_days(row, AMOUNT, 7, lambda a, b: abs(a - b) <= 20)
+        same_amount_last_month = same_attr_past_n_days(row, AMOUNT, 30, lambda a, b: abs(a - b) <= 20)
+        same_categoryid_last_week = same_attr_past_n_days(row, CATEGORYID, 7, lambda a, b: a == b)
+        same_categoryid_last_month = same_attr_past_n_days(row, CATEGORYID, 30, lambda a, b: a == b)
+        additional_features_data.append([total_incomes_last_week,
+                                         num_of_incomes_last_week,
+                                         total_incomes_last_month,
+                                         num_of_incomes_last_month,
+                                         same_name_last_week,
+                                         same_name_last_month,
+                                         same_amount_last_week,
+                                         same_amount_last_month,
+                                         same_categoryid_last_week,
+                                         same_categoryid_last_month])
 
-    # subscription prediction features
-    transactions_df[SAME_NAME_LAST_MONTH] = ""
-    transactions_df[SAME_CATEGORYID_LAST_WEEK] = ""
-    transactions_df[SAME_CATEGORYID_LAST_MONTH] = ""
-    transactions_df[SAME_AMOUNT_LAST_WEEK] = ""
-    transactions_df[SAME_AMOUNT_LAST_MONTH] = ""
-    print ""
-    # subscription:
-    # 1. same categoryId and name
-    # 2. once every 7 days or 30 days
-    # 3. deviation <= 20$ from average amount
+    additional_features_df = pd.DataFrame(additional_features_data, columns=[TOTAL_INCOMES_LAST_WEEK,
+                                                                             NUM_OF_INCOMES_LAST_WEEK,
+                                                                             TOTAL_INCOMES_LAST_MONTH,
+                                                                             NUM_OF_INCOMES_LAST_MONTH,
+                                                                             SAME_NAME_LAST_WEEK,
+                                                                             SAME_NAME_LAST_MONTH,
+                                                                             SAME_AMOUNT_LAST_WEEK,
+                                                                             SAME_AMOUNT_LAST_MONTH,
+                                                                             SAME_CATEGORYID_LAST_WEEK,
+                                                                             SAME_CATEGORYID_LAST_MONTH])
+
+    all_data_df = pd.concat([transactions_df, additional_features_df], axis=1)
+    all_data_df[SUBSCRIPTION] = \
+        (all_data_df[SAME_NAME_LAST_WEEK] & all_data_df[SAME_AMOUNT_LAST_WEEK] & all_data_df[SAME_CATEGORYID_LAST_WEEK]) | \
+        (all_data_df[SAME_NAME_LAST_MONTH] & all_data_df[SAME_AMOUNT_LAST_MONTH] & all_data_df[SAME_CATEGORYID_LAST_MONTH])
+
+    return all_data_df
 
 
 def get_last_n_days_incomes(row, n):
@@ -84,14 +111,13 @@ def get_last_n_days_incomes(row, n):
     return last_n_days_incomes
 
 
-def same_attr_past_n_days(row, attr, n, similarity=1.0):
+def same_attr_past_n_days(row, attr, n, compare_func):
     last_n_days_transaction = transactions_df[(transactions_df[USERID] == row[USERID]) &
                                               (transactions_df[INCOME] == False) &
                                               (transactions_df[DATE] == row[DATE] - pd.DateOffset(days=n))]
-    return last_n_days_transaction[attr].apply(
-        lambda x: SequenceMatcher(None, x, row[attr]).ratio() >= similarity).any()
+    return last_n_days_transaction[attr].apply(lambda x: compare_func(x, row[attr])).any()
 
 
 if __name__ == '__main__':
     read_users_and_transactions_files("./users.json", "./transactions_clean.json")
-    part_a()
+    all_data = part_a()
